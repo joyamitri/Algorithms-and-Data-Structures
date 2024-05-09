c7ca5fa108c73c38d1dfd0873f3054b4ad47bd76
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2024-05-09 20:27:52 +0300

    Creating <> Node class

---
+++ GaleShapleyAlgo/Node.java
@@ -0,0 +1,25 @@
+package GaleShapleyAlgo;
+
+public class Node {
+    private int info;
+	private Node next;
+	public Node(int i){
+		info=i;
+		next=null;
+	}
+	public int getInfo() {
+		return info;
+	}
+	public void setInfo(int i) {
+		info = i;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node n) {
+		next = n;
+	}
+	public String toString(){
+		return "Info: "+info;
+	}
+}
